104b292b1470e1e0845266155e3368060f8b094a
Balamuruganhit <balamuruganb011@gmail.com>
2024-09-04 21:08:25 +0530

    change in compose up

--- compose-up.sh
+++ compose-up.sh
@@ -8,5 +8,5 @@ if [[ ! $1 ]]; then
   exit 1
 fi
 sed -i -r 's/DB_TYPE=(mysql|postgres)/DB_TYPE='$1'/g' config.env 
-docker-compose -f docker-$1-compose.yml up --build -d
+docker compose -f docker-$1-compose.yml up --build -d
 
